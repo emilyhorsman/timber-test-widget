660db2e0a568ceb74423a12184cbfa297ee2eb08
Emily Horsman <me@emilyhorsman.com>
2015-08-30 14:00:41 -0400

    Enqueue a stylesheet for the key/value table

--- timber-test-widget.php
+++ timber-test-widget.php
@@ -12,7 +12,10 @@ namespace buttercup;
 
 defined('ABSPATH') or die('-1');
 
-\Timber::$locations[] = path_join(plugin_dir_path(__FILE__), 'templates');
+function add_plugin_template_folder_to_timber() {
+  \Timber::$locations[] = path_join(plugin_dir_path(__FILE__), 'templates');
+}
+add_plugin_template_folder_to_timber();
 
 require_once('includes/timber-test-widget.class.php');
 require_once('includes/timber-test-widget-admin-notice.class.php');
@@ -22,3 +25,11 @@ add_action('widgets_init', function() {
 });
 
 add_action('admin_notices', __NAMESPACE__ . '\\Admin_Notice::hello_world');
+
+function add_theme_styles() {
+  wp_enqueue_style(
+    'timber-test-widget',
+    plugins_url('assets/css/timber-test-widget.css', __FILE__)
+  );
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\add_theme_styles');
